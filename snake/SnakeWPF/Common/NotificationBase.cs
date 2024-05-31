8a21b68bccc7782871d795351e9d5bf50491e595
thirdsqueak <lvasilkov952@gmail.com>
2024-05-31 14:35:27 +0300

    first commit

---
+++ snake/SnakeWPF/Common/NotificationBase.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SnakeWPF.Common
+{
+    /// <summary>
+    /// The NotificationBase class represents an implementation of the INotifyPropertyChanged interface.
+    /// It is used by classes that need to raise a property changed event.
+    /// </summary>
+    public class NotificationBase : INotifyPropertyChanged
+    {
+        #region Fields
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The RaisePropertyChanged method is called raise a property changed event. 
+        /// </summary>
+        /// <param name="propertyname"></param>
+        protected void RaisePropertyChanged([CallerMemberName] string propertyname = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+        #endregion
+    }
+}
